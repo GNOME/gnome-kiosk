bea5318ceed92ef8fc0852f5094d8d4f90af2709
Ray Strode <rstrode@redhat.com>
2021-03-17 11:00:08 -0400

    build: Add new gobject utilities
    One thing we're going to need soon is the ability to dispatch a function
    as the result of a flurry of events happening.  Any of the events should
    lead to the function getting called, but we don't want to run it more
    than once when several events happen at roughly the same time.
    
    This commit adds a utility function for that purpose. Upon getting
    called, it waits a 250ms coalescing interval before invoking the passed
    in callback.  If the utility function is invoked during the coalescing
    interval of a prior call, the function call is ignored.
    
    For convenience, the callback function has two data arguments, the
    first is the object associated with the call, and the second is
    arbitrary.

---
+++ compositor/kiosk-gobject-utils.c
@@ -0,0 +1,83 @@
+#include "config.h"
+#include "kiosk-gobject-utils.h"
+
+#define COALESCE_INTERVAL 250 /* milliseconds */
+
+static void
+on_task_wait_complete (GObject *self,
+                       GTask   *task)
+{
+        KioskObjectCallback callback;
+        gpointer user_data;
+        gboolean completed;
+        g_autofree char *data_key = NULL;
+
+        callback = g_object_get_data (G_OBJECT (task), "callback");
+        user_data = g_object_get_data (G_OBJECT (task), "user-data");
+
+        completed = g_task_propagate_boolean (task, NULL);
+
+        if (completed) {
+                callback (self, user_data);
+        }
+
+        data_key = g_strdup_printf ("kiosk-gobject-utils-%p-%p-task",
+                                    callback, user_data);
+
+        g_object_set_data (G_OBJECT (self), data_key, NULL);
+}
+
+static gboolean
+on_coalesce_timeout (GTask *task)
+{
+        if (!g_task_return_error_if_cancelled (task)) {
+                g_task_return_boolean (task, TRUE);
+        }
+
+        return G_SOURCE_REMOVE;
+}
+
+void
+kiosk_gobject_utils_queue_defer_callback (GObject             *self,
+                                          const char          *name,
+                                          GCancellable        *cancellable,
+                                          KioskObjectCallback  callback,
+                                          gpointer             user_data)
+{
+        g_autofree char *data_key = NULL;
+        g_autoptr (GSource) timeout_source = NULL;
+        GTask *task;
+
+        g_return_if_fail (G_IS_OBJECT (self));
+        g_return_if_fail (callback != NULL);
+
+        data_key = g_strdup_printf ("kiosk-gobject-utils-%p-%p-task",
+                                    callback, user_data);
+
+        task = g_object_get_data (G_OBJECT (self), data_key);
+
+        if (task != NULL) {
+                return;
+        }
+
+        timeout_source = g_timeout_source_new (COALESCE_INTERVAL);
+
+        if (name != NULL) {
+                g_source_set_name (timeout_source, name);
+        }
+
+        task = g_task_new (self,
+                           cancellable,
+                           (GAsyncReadyCallback) on_task_wait_complete,
+                           NULL);
+        g_task_attach_source (task, timeout_source, G_SOURCE_FUNC (on_coalesce_timeout));
+
+        g_object_set_data (G_OBJECT (task), "callback", callback);
+        g_object_set_data (G_OBJECT (task), "user-data", user_data);
+
+        g_object_set_data_full (G_OBJECT (self),
+                                data_key,
+                                task,
+                                (GDestroyNotify)
+                                g_object_unref);
+}
---
+++ compositor/kiosk-gobject-utils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+typedef void (* KioskObjectCallback) (GObject *self, gpointer user_data);
+#define KIOSK_OBJECT_CALLBACK(_callback) ((KioskObjectCallback) _callback)
+
+void kiosk_gobject_utils_queue_defer_callback (GObject             *self,
+                                               const char          *name,
+                                               GCancellable        *cancellable,
+                                               KioskObjectCallback  callback,
+                                               gpointer             user_data);
+
+G_END_DECLS
