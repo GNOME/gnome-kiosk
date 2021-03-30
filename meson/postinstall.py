0818e17f0f8a32c53600ee167676dc66dd09b458
Ray Strode <rstrode@redhat.com>
2021-03-30 15:09:47 -0400

    Add an example search appliance
    In order to facilitate testing during development, and in order
    to demonstate how the kiosk compositor is used, we need a simple
    application.
    
    This commit adds a search appliance that just shows a full screen
    google window.

--- meson/postinstall.py
+++ meson/postinstall.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import subprocess
+import sys
 
+destdir = os.environ.get('DESTDIR', '/')
 prefix = os.environ.get('MESON_INSTALL_PREFIX', '/usr/local')
-datadir = os.path.join(prefix, 'share')
+datadir = os.path.join(destdir + prefix, 'share')
+
+wayland_sessions_dir = os.path.join(datadir, 'wayland-sessions')
+if not os.path.exists(wayland_sessions_dir):
+    os.makedirs(wayland_sessions_dir)
+
+source_file = os.path.join(datadir, 'xsessions', 'org.gnome.Kiosk.SearchApp.Session.desktop')
+destination_file = os.path.join(wayland_sessions_dir, 'org.gnome.Kiosk.SearchApp.Session.desktop')
+shutil.copyfile(source_file, destination_file)
 
 # Packaging tools define DESTDIR and this isn't needed for them
 if 'DESTDIR' not in os.environ:
