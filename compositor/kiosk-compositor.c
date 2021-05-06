ed8dbeb00ef64faa047652ac7fc9bf029348a6df
Ray Strode <rstrode@redhat.com>
2021-05-06 14:17:26 -0400

    compositor: Put the on top windows actually on top
    A typo in the code left me putting new windows under old ones.
    
    This commit fixes that.

--- compositor/kiosk-compositor.c
+++ compositor/kiosk-compositor.c
@@ -266,7 +266,7 @@ kiosk_compositor_map (MetaPlugin      *plugin,
                         g_object_ref (G_OBJECT (actor));
                         clutter_actor_remove_child (clutter_actor_get_parent (CLUTTER_ACTOR (actor)), CLUTTER_ACTOR (actor));
                         clutter_actor_add_child (window_group, CLUTTER_ACTOR (actor));
-                        clutter_actor_set_child_below_sibling (window_group, CLUTTER_ACTOR (actor), NULL);
+                        clutter_actor_set_child_above_sibling (window_group, CLUTTER_ACTOR (actor), NULL);
                         g_object_unref (G_OBJECT (actor));
                 }
 
