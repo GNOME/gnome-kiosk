GNOME/gnome-kiosk

commit ae23ec4e483bc12f24bd81f6cba93570af662b30

input-sources-manager: Support libxklavier managed keyboard layouts
Mutter currently doesn't allow libxklavier to change the layout out from
under it.

This commit fixes that on X being careful to make sure the current layout
state, as gnome-kiosk sees it, stays in sync with X server state.

compositor/kiosk-input-source-group.c
@@ -24,6 +24,7 @@ struct _KioskInputSourceGroup
         /* weak references */
         KioskInputSourcesManager *input_sources_manager;
         KioskInputEngineManager *input_engine_manager;
+        KioskXKeyboardManager *x_keyboard_manager;
 
         /* strong references */
         char *input_engine_name;
@@ -34,7 +35,6 @@ struct _KioskInputSourceGroup
         /* state */
         xkb_layout_index_t layout_index;
 };
-
 enum
 {
   PROP_INPUT_SOURCES_MANAGER = 1,
@@ -240,12 +240,20 @@ kiosk_input_source_group_set_options (KioskInputSourceGroup *self,
         self->options = g_strdup (options);
 }
 
+const char *
+kiosk_input_source_group_get_options (KioskInputSourceGroup *self)
+{
+        return self->options;
+}
+
 gboolean
 kiosk_input_source_group_activate (KioskInputSourceGroup *self)
 {
         size_t number_of_layouts;
         g_autofree char *layouts = NULL;
         g_autofree char *variants = NULL;
+        gboolean keymap_already_set = FALSE;
+        gboolean layout_group_already_locked = FALSE;
 
         g_debug ("KioskInputSourceGroup: Activating input source");
 
@@ -275,11 +283,27 @@ kiosk_input_source_group_activate (KioskInputSourceGroup *self)
                 kiosk_input_engine_manager_activate_engine (self->input_engine_manager, NULL);
         }
 
-        g_debug ("KioskInputSourceGroup: Setting keyboard mapping to [%s] (%s) [%s]",
-                 layouts, variants, self->options);
+        if (self->x_keyboard_manager != NULL) {
+                keymap_already_set = kiosk_x_keyboard_manager_keymap_is_active (self->x_keyboard_manager, (const char * const *) self->layouts->pdata, (const char * const *) self->variants->pdata, self->options);
+                layout_group_already_locked = kiosk_x_keyboard_manager_layout_group_is_locked (self->x_keyboard_manager, self->layout_index);
 
-        meta_backend_set_keymap (meta_get_backend (), layouts, variants, self->options);
-        meta_backend_lock_layout_group (meta_get_backend (), self->layout_index);
+        }
+
+        if (!keymap_already_set) {
+                g_debug ("KioskInputSourceGroup: Setting keyboard mapping to [%s] (%s) [%s]",
+                         layouts, variants, self->options);
+
+                meta_backend_set_keymap (meta_get_backend (), layouts, variants, self->options);
+        }
+
+        if (!layout_group_already_locked) {
+                g_debug ("KioskInputSourceGroup: Locking layout to index %d", self->layout_index);
+                meta_backend_lock_layout_group (meta_get_backend (), self->layout_index);
+        }
+
+        if (keymap_already_set && layout_group_already_locked) {
+                g_debug ("KioskInputSourceGroup: Input source already active");
+        }
 
         return TRUE;
 }
@@ -299,6 +323,17 @@ get_index_of_layout (KioskInputSourceGroup *self,
         }
 
         return -1;
+}
+
+gboolean
+kiosk_input_source_group_only_has_layouts (KioskInputSourceGroup *self,
+                                           const char * const    *layouts_to_check)
+{
+        g_auto (GStrv) layouts;
+
+        layouts = kiosk_input_source_group_get_layouts (self);
+
+        return g_strv_equal (layouts_to_check, (const char * const *) layouts);
 }
 
 gboolean
@@ -519,6 +554,7 @@ kiosk_input_source_group_constructed (GObject *object)
         G_OBJECT_CLASS (kiosk_input_source_group_parent_class)->constructed (object);
 
         g_set_weak_pointer (&self->input_engine_manager, kiosk_input_sources_manager_get_input_engine_manager (self->input_sources_manager));
+        g_set_weak_pointer (&self->x_keyboard_manager, kiosk_input_sources_manager_get_x_keyboard_manager (self->input_sources_manager));
 }
 
 static void
@@ -533,6 +569,7 @@ kiosk_input_source_group_dispose (GObject *object)
         g_clear_pointer (&self->variants, g_ptr_array_unref);
         g_clear_pointer (&self->layouts, g_ptr_array_unref);
 
+        g_clear_weak_pointer (&self->x_keyboard_manager);
         g_clear_weak_pointer (&self->input_engine_manager);
         g_clear_weak_pointer (&self->input_sources_manager);
 

compositor/kiosk-input-source-group.h
@@ -25,10 +25,13 @@ gboolean kiosk_input_source_group_set_input_engine (KioskInputSourceGroup *input
 const char *kiosk_input_source_group_get_input_engine (KioskInputSourceGroup *input_sources);
 
 void kiosk_input_source_group_set_options (KioskInputSourceGroup *input_sources,
-                                     const char       *options);
+                                           const char            *options);
+const char *kiosk_input_source_group_get_options (KioskInputSourceGroup *self);
 
 gboolean kiosk_input_source_group_activate (KioskInputSourceGroup *input_sources);
 
+gboolean kiosk_input_source_group_only_has_layouts (KioskInputSourceGroup *self,
+                                                    const char * const    *layouts_to_check);
 gboolean kiosk_input_source_group_switch_to_layout (KioskInputSourceGroup *input_sources,
                                                     const char            *layout_name);
 void kiosk_input_source_group_switch_to_first_layout (KioskInputSourceGroup *input_sources);

compositor/kiosk-input-sources-manager.c
@@ -22,6 +22,7 @@
 #include "kiosk-gobject-utils.h"
 #include "kiosk-input-engine-manager.h"
 #include "kiosk-input-source-group.h"
+#include "kiosk-x-keyboard-manager.h"
 
 #define SD_LOCALE1_BUS_NAME "org.freedesktop.locale1"
 #define SD_LOCALE1_OBJECT_PATH "/org/freedesktop/locale1"
@@ -51,6 +52,7 @@ struct _KioskInputSourcesManager
         /* strong references */
         GCancellable *cancellable;
         KioskInputEngineManager *input_engine_manager;
+        KioskXKeyboardManager *x_keyboard_manager;
         SdLocale1 *locale_proxy;
         GnomeXkbInfo *xkb_info;
         GSettings *input_sources_settings;
@@ -717,6 +719,12 @@ kiosk_input_sources_manager_get_input_engine_manager (KioskInputSourcesManager *
         return self->input_engine_manager;
 }
 
+KioskXKeyboardManager *
+kiosk_input_sources_manager_get_x_keyboard_manager (KioskInputSourcesManager *self)
+{
+        return self->x_keyboard_manager;
+}
+
 void
 kiosk_input_sources_manager_clear_input_sources (KioskInputSourcesManager *self)
 {
@@ -1368,6 +1376,126 @@ kiosk_input_sources_manager_start_input_engine_manager (KioskInputSourcesManager
 }
 
 static void
+process_x_keyboard_manager_selected_layout_change (KioskInputSourcesManager *self)
+{
+        const char *selected_layout;
+
+        selected_layout = kiosk_x_keyboard_manager_get_selected_layout (self->x_keyboard_manager);
+
+        if (selected_layout == NULL) {
+                return;
+        }
+
+        g_debug ("KioskInputSourcesManager: X server changed active layout to %s", selected_layout);
+
+        activate_input_source_group_with_layout (self, selected_layout);
+
+        sync_dbus_service (self);
+}
+
+static void
+on_x_keyboard_manager_selected_layout_changed (KioskInputSourcesManager *self)
+{
+        /* We defer processing the layout change for a bit, because often in practice there is more than
+         * one layout change at the same time, and only the last one is the desired one
+         */
+        kiosk_gobject_utils_queue_defer_callback (G_OBJECT (self),
+                                                  "[kiosk-input-sources-manager] process_x_keyboard_manager_selected_layout_change",
+                                                  self->cancellable,
+                                                  KIOSK_OBJECT_CALLBACK (process_x_keyboard_manager_selected_layout_change),
+                                                  NULL);
+}
+
+static gboolean
+layouts_match_selected_input_source_group (KioskInputSourcesManager  *self,
+                                           const char * const        *layouts,
+                                           const char                *options)
+{
+        KioskInputSourceGroup *input_source_group;
+
+        g_auto (GStrv) current_layouts = NULL;
+        const char *input_source_group_options;
+        const char *input_engine_name;
+
+        input_source_group = kiosk_input_sources_manager_get_selected_input_source_group (self);
+
+        if (input_source_group == NULL) {
+                return FALSE;
+        }
+
+        input_engine_name = kiosk_input_source_group_get_input_engine (input_source_group);
+
+        if (input_engine_name != NULL) {
+                return FALSE;
+        }
+
+        current_layouts = kiosk_input_source_group_get_layouts (input_source_group);
+
+        if (!g_strv_equal ((const char * const *) current_layouts, layouts)) {
+                return FALSE;
+        }
+
+        input_source_group_options = kiosk_input_source_group_get_options (input_source_group);
+
+        if (g_strcmp0 (input_source_group_options, options) != 0) {
+                return FALSE;
+        }
+
+        return TRUE;
+}
+
+static void
+on_x_keyboard_manager_layouts_changed (KioskInputSourcesManager *self)
+{
+        const char * const *new_layouts;
+        const char *selected_layout;
+        const char *options;
+        gboolean layouts_match;
+        size_t i;
+
+        new_layouts = kiosk_x_keyboard_manager_get_layouts (self->x_keyboard_manager);
+        options = kiosk_x_keyboard_manager_get_options (self->x_keyboard_manager);
+        layouts_match = layouts_match_selected_input_source_group (self, new_layouts, options);
+
+        if (layouts_match) {
+                return;
+        }
+
+        g_debug ("KioskInputSorcesManager: X server keyboard layouts changed");
+
+        self->overriding_configuration = TRUE;
+        kiosk_input_sources_manager_clear_input_sources (self);
+
+        for (i = 0; new_layouts[i] != NULL; i++) {
+                kiosk_input_sources_manager_add_layout (self, new_layouts[i], options);
+        }
+
+        selected_layout = kiosk_x_keyboard_manager_get_selected_layout (self->x_keyboard_manager);
+
+        if (selected_layout != NULL) {
+                activate_best_available_input_source_group (self, NULL, selected_layout);
+        }
+}
+
+static void
+kiosk_input_source_manager_start_x_keyboard_manager (KioskInputSourcesManager *self)
+{
+        g_debug ("KioskInputSourcesManager: Starting X Keyboard Manager");
+        self->x_keyboard_manager = kiosk_x_keyboard_manager_new (self->compositor);
+
+        g_signal_connect_object (G_OBJECT (self->x_keyboard_manager),
+                                 "notify::selected-layout",
+                                 G_CALLBACK (on_x_keyboard_manager_selected_layout_changed),
+                                 self,
+                                 G_CONNECT_SWAPPED);
+        g_signal_connect_object (G_OBJECT (self->x_keyboard_manager),
+                                 "notify::layouts",
+                                 G_CALLBACK (on_x_keyboard_manager_layouts_changed),
+                                 self,
+                                 G_CONNECT_SWAPPED);
+}
+
+static void
 kiosk_input_sources_manager_handle_dbus_service (KioskInputSourcesManager *self)
 {
         KioskService *service;
@@ -1434,6 +1562,20 @@ kiosk_input_sources_manager_constructed (GObject *object)
         kiosk_input_sources_manager_add_key_bindings (self);
 
         kiosk_input_sources_manager_set_input_sources_from_session_configuration (self);
+
+        /* We start the X keyboard manager after we've already loaded and locked in
+         * GSettings etc, so the session settings take precedence over xorg.conf
+         */
+        if (!meta_is_wayland_compositor ()) {
+                g_debug ("KioskInputSourcesManager: Will start X keyboard manager shortly");
+                kiosk_gobject_utils_queue_defer_callback (G_OBJECT (self),
+                                                          "[kiosk-input-sources-manager] kiosk_input_source_manager_start_x_keyboard_manager",
+                                                          self->cancellable,
+                                                          KIOSK_OBJECT_CALLBACK (kiosk_input_source_manager_start_x_keyboard_manager),
+                                                          NULL);
+        } else {
+                g_debug ("KioskInputSourcesManager: Won't start X keyboard manager on wayland");
+        }
 }
 
 static void
@@ -1454,6 +1596,7 @@ kiosk_input_sources_manager_dispose (GObject *object)
         kiosk_input_sources_manager_clear_input_sources (self);
 
         g_clear_object (&self->input_engine_manager);
+        g_clear_object (&self->x_keyboard_manager);
 
         g_clear_object (&self->xkb_info);
 

compositor/kiosk-input-sources-manager.h
@@ -4,6 +4,7 @@
 
 #include "kiosk-input-source-group.h"
 #include "kiosk-input-engine-manager.h"
+#include "kiosk-x-keyboard-manager.h"
 
 typedef struct _KioskCompositor KioskCompositor;
 
@@ -18,6 +19,7 @@ G_DECLARE_FINAL_TYPE (KioskInputSourcesManager,
 
 KioskInputSourcesManager *kiosk_input_sources_manager_new (KioskCompositor *compositor);
 KioskInputEngineManager *kiosk_input_sources_manager_get_input_engine_manager (KioskInputSourcesManager *manager);
+KioskXKeyboardManager *kiosk_input_sources_manager_get_x_keyboard_manager (KioskInputSourcesManager *manager);
 
 void kiosk_input_sources_manager_clear_input_sources (KioskInputSourcesManager *self);
 gboolean kiosk_input_sources_manager_set_input_sources_from_locales (KioskInputSourcesManager *self,

compositor/kiosk-x-keyboard-manager.h
@@ -1,0 +1,29 @@
+#pragma once
+
+#include <glib-object.h>
+#include <xkbcommon/xkbcommon.h>
+
+typedef struct _KioskCompositor KioskCompositor;
+
+G_BEGIN_DECLS
+
+#define KIOSK_TYPE_X_KEYBOARD_MANAGER (kiosk_x_keyboard_manager_get_type ())
+
+G_DECLARE_FINAL_TYPE (KioskXKeyboardManager,
+                      kiosk_x_keyboard_manager,
+                      KIOSK, X_KEYBOARD_MANAGER,
+                      GObject)
+
+KioskXKeyboardManager *kiosk_x_keyboard_manager_new (KioskCompositor *compositor);
+
+const char * const *kiosk_x_keyboard_manager_get_layouts (KioskXKeyboardManager *manager);
+const char *kiosk_x_keyboard_manager_get_selected_layout (KioskXKeyboardManager *manager);
+const char *kiosk_x_keyboard_manager_get_options (KioskXKeyboardManager *manager);
+gboolean kiosk_x_keyboard_manager_keymap_is_active (KioskXKeyboardManager *manager,
+                                                    const char * const    *layouts,
+                                                    const char * const    *variants,
+                                                    const char            *options);
+gboolean kiosk_x_keyboard_manager_layout_group_is_locked (KioskXKeyboardManager *manager,
+                                                          xkb_layout_index_t     layout_index);
+
+G_END_DECLS

meson.build
@@ -113,6 +113,7 @@ compositor_sources += 'compositor/kiosk-input-engine-manager.c'
 compositor_sources += 'compositor/kiosk-input-source-group.c'
 compositor_sources += 'compositor/kiosk-service.c'
 compositor_sources += 'compositor/kiosk-shell-service.c'
+compositor_sources += 'compositor/kiosk-x-keyboard-manager.c'
 compositor_sources += 'compositor/main.c'
 
 foreach dbus_interface, sources: dbus_interface_sources_map

commit e38ac4eb960821c544da8760c87e53ccc4cbbc74

input-source-group: Fix compiler warning
Initialize the g_auto variable to NULL to avoid a compiler warning:

 | In file included from /usr/lib64/glib-2.0/include/glibconfig.h:9,
 |               from /usr/include/glib-2.0/glib/gtypes.h:34,
 |               from /usr/include/glib-2.0/glib/galloca.h:34,
 |               from /usr/include/glib-2.0/glib.h:32,
 |               from /usr/include/glib-2.0/gobject/gbinding.h:30,
 |               from /usr/include/glib-2.0/glib-object.h:24,
 |               from compositor/kiosk-input-source-group.h:3,
 |               from compositor/kiosk-input-source-group.c:2:
 | In function ‘glib_auto_cleanup_GStrv’,
 |   inlined from ‘kiosk_input_source_group_only_has_layouts’ at
 |   compositor/kiosk-input-source-group.c:347:24:
 | /usr/include/glib-2.0/glib/gmacros.h:1379:89: warning: ‘layouts’ may
 |   be used uninitialized [-Wmaybe-uninitialized]
 | 1379 |   static G_GNUC_UNUSED inline void _GLIB_AUTO_FUNC_NAME(TypeName)
 |      |     (TypeName *_ptr) { if (*_ptr != none) (func) (*_ptr); }     \
 |      |                                                        ^
 | compositor/kiosk-input-source-group.c: In function
 |  ‘kiosk_input_source_group_only_has_layouts’:
 | compositor/kiosk-input-source-group.c:347:24: note: ‘layouts’ was declared here
 | 347 |         g_auto (GStrv) layouts;
 |     |                        ^~~~~~~

Fixes: ae23ec4e - input-sources-manager: Support libxklavier managed keyboard layouts
Part-of: <https://gitlab.gnome.org/GNOME/gnome-kiosk/-/merge_requests/68>

compositor/kiosk-input-source-group.c
@@ -344,7 +344,7 @@ gboolean
 kiosk_input_source_group_only_has_layouts (KioskInputSourceGroup *self,
                                            const char * const    *layouts_to_check)
 {
-        g_auto (GStrv) layouts;
+        g_auto (GStrv) layouts = NULL;
 
         layouts = kiosk_input_source_group_get_layouts (self);
 